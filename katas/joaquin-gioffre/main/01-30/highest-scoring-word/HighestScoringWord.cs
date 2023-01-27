c3aae4d7236b0c054f17b47b3efa23758dd0930c
Joaquin Tomas Gioffre <j.gioffre@hotmail.com>
2023-01-27 14:13:38 -0300

    Added .gitignore file; added highestScoringword kata solution with unit test

---
+++ katas/joaquin-gioffre/main/01-30/highest-scoring-word/HighestScoringWord.cs
@@ -0,0 +1,40 @@
+namespace HighestScoringWord;
+
+using System.Collections.Generic;
+
+public class HighestScoringWord
+{
+    string Word = "";
+    int HighestScore = 0;
+
+    public int GetWordScore(string word, Dictionary<string, int> alphabet)
+    {
+        int score = 0;
+        foreach(char character in word)
+        {
+            if(alphabet.ContainsKey(character.ToString()))
+            {
+                score += alphabet[character.ToString()];
+            }
+        }
+
+        return score;
+    }
+
+    public string GetHighestScoringWord(string words, Dictionary<string, int> alphabet)
+    {
+        string[] allWords = words.Split(" ").ToArray();
+        foreach(string w in allWords)
+        {
+            if(GetWordScore(w, alphabet) > HighestScore)
+            {
+                Word = w;
+                HighestScore = GetWordScore(w, alphabet);
+            }
+        }
+        HighestScore = 0;
+
+        return Word;
+    }
+    
+}
