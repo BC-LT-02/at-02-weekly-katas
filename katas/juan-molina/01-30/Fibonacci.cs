10dc828bbaba528ff55ec62d5b9a77073159930a
Sch <Schlvf>
2023-01-27 12:56:33 -0500

    Implemented 3 new katas

---
+++ katas/juan-molina/01-30/Fibonacci.cs
@@ -0,0 +1,15 @@
+﻿//https://www.codewars.com/kata/522551eee9abb932420004a0/csharp
+public class Fibonacci
+{
+    public int NthFib(int n)
+    {
+        int curr = 1;
+        int prev = 0;
+        for (int i = 0; i < n - 2; i++)
+        {
+            curr = curr + prev;
+            prev = curr - prev;
+        }
+        return (n > 1) ? curr : 0;
+    }
+}
---
+++ katas/juan-molina/01-30/Narcissistic.cs
@@ -0,0 +1,29 @@
+﻿//https://www.codewars.com/kata/5287e858c6b5a9678200083c/csharp
+using System.Linq;
+using System;
+
+public class Kata
+{
+    public static bool Narcissistic(int value)
+    {
+        char[] chars = value.ToString().ToArray();
+
+        int count = 0;
+        foreach (char x in chars)
+        {
+            count += pow((int)Char.GetNumericValue(x), chars.Count());
+        }
+
+        return count == value;
+    }
+
+    public static int pow(int nBase, int power)
+    {
+        int res = 1;
+        for (int i = 0; i < power; i++)
+        {
+            res *= nBase;
+        }
+        return res;
+    }
+}
